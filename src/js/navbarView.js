90b9050b5ce78f8c616bc31db9885bb9771c603c
markpedong <mark.pedong01@gmail.com>
2022-02-05 18:18:08 +0800

    Finished FAQ Section

--- src/js/navbarView.js
+++ src/js/navbarView.js
@@ -4,6 +4,20 @@ import { OFFSET_SCREEN } from "./config.js";
 const nav = document.querySelector(".navbar");
 const btnOpen = document.querySelector(".navbar-toggler");
 const navLinks = document.querySelectorAll(".nav-link");
+const accordion = document.querySelector(".accordion__3");
+
+// Accordion 3 Change Border Bottom Color
+let index = 0;
+
+const colors = ["white", "rgba(0, 0, 0, 0.5)"];
+
+accordion.addEventListener("click", function onClick() {
+  accordion.style.borderBottom = `2px solid ${colors[index]}`;
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
+
+///////////////////////////////////////////////////////
 
 function navColorChange() {
   window.addEventListener("scroll", function () {
