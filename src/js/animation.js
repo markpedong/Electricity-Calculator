fc9b6e5c44b294227de6f0d78d6be2889be0308a
markpedong <mark.pedong01@gmail.com>
2022-02-07 12:11:19 +0800

    Added Animation in between sections

--- src/js/animation.js
+++ src/js/animation.js
@@ -4,15 +4,24 @@ const masthead = document.querySelector(".masthead");
 const navLinks = document.querySelector(".navbar-nav");
 const navBrand = document.querySelector(".navbar-brand");
 const header = document.querySelector(".headline");
+const calcContainer = document.querySelector(".calculator");
+const deviceApp = document.querySelector(".device__appliances");
+const deviceCalc = document.querySelector(".device__calculator");
+const secFAQ = document.querySelector(".sec__faq");
+const faqContainer = document.querySelector(".faq__container");
+const secContact = document.querySelector(".sec__contact");
+const contactContainer = document.querySelector(".contact__container");
 
-const tl = new TimelineMax();
+// Landing Page Animation
+const landingTl = new TimelineMax();
 
-tl.fromTo(
-  imgTextContainer,
-  1,
-  { height: "0%" },
-  { height: "70%", ease: Power2.easeInOut }
-)
+landingTl
+  .fromTo(
+    imgTextContainer,
+    1,
+    { height: "0%" },
+    { height: "70%", ease: Power2.easeInOut }
+  )
   .fromTo(
     imgTextContainer,
     1.2,
@@ -29,3 +38,65 @@ tl.fromTo(
   .fromTo(navBrand, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
   .fromTo(navLinks, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
   .fromTo(navBrand, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
+
+// Section Calculator Animation
+
+let calcTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: calcContainer,
+    start: "center bottom",
+  },
+});
+
+calcTl
+  .from(deviceApp, {
+    x: -300,
+    opacity: 0,
+    duration: 1.5,
+    ease: Power2.easeInOut,
+  })
+  .from(
+    deviceCalc,
+    { x: 300, opacity: 0, duration: 1.2, ease: Power2.easeInOut },
+    "-=1.2"
+  );
+
+// Section FAQ Animation
+
+let faqTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: secFAQ,
+    start: "center bottom",
+  },
+});
+
+faqTl
+  .from(secFAQ, { x: -2000, duration: 2, ease: Power2.easeInOut })
+  .from(
+    faqContainer,
+    { y: 500, opacity: 0, duration: 1.2, ease: Power2.easeInOut },
+    "+1.5"
+  );
+
+// Section Contact Animation
+
+let contactTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: secContact,
+    start: "center bottom",
+  },
+});
+
+contactTl
+  .fromTo(
+    secContact,
+    1.2,
+    { x: "100%" },
+    { x: "0%", ease: Power2.easeInOut },
+    "-="
+  )
+  .from(
+    contactContainer,
+    { x: 1000, opacity: 0, duration: 1.2, ease: Power2.easeInOut },
+    "+1.5"
+  );
--- src/js/navbarView.js
+++ src/js/navbarView.js
@@ -3,6 +3,12 @@
 const btnOpen = document.querySelector(".navbar-toggler");
 const navLinks = document.querySelectorAll(".nav-link");
 const accordion = document.querySelector(".accordion__3");
+const navCalculator = document.querySelector(".nav__calculator");
+const secCalculator = document.querySelector(".calculator");
+const navFAQ = document.querySelector(".nav__faq");
+const secFAQ = document.querySelector(".sec__faq");
+const navContact = document.querySelector(".nav__contact");
+const secContact = document.querySelector(".sec__contact");
 
 // Accordion 3 Change Border Bottom Color
 let index = 0;
@@ -20,4 +26,16 @@ btnOpen.addEventListener("click", function () {
     e.classList.remove("text-white");
     e.classList.add("text-black");
   });
+});
+
+navCalculator.addEventListener("click", function () {
+  secCalculator.scrollIntoView({ behavior: "smooth" });
+});
+
+navFAQ.addEventListener("click", function () {
+  secFAQ.scrollIntoView({ behavior: "smooth" });
+});
+
+navContact.addEventListener("click", function () {
+  secContact.scrollIntoView({ behavior: "smooth" });
 });
